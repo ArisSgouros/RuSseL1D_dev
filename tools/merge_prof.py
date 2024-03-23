7c80279ce6073593b85897468db5845ced7bc3b0
ArisSgouros <arissgouros@gmail.com>
2024-03-23 08:35:57 +0200

    feat(tool/merge_prof): add new processing capabilities

--- tools/merge_prof.py
+++ tools/merge_prof.py
@@ -1,10 +1,23 @@
 import numpy as np
 import sys
 import os
+import argparse
 
-file_tag = sys.argv[1]
-x_col = int(sys.argv[2])
-y_col = int(sys.argv[3])
+parser = argparse.ArgumentParser()
+parser.add_argument('file_tag', type=str, help='common file tag')
+parser.add_argument('x_col',  type=int, help='x column')
+parser.add_argument('y_col',  type=int, help='y column')
+parser.add_argument('-shift',  type=float, default=0.0, help='')
+parser.add_argument('-scale',  type=float, default=1.0, help='')
+parser.add_argument('-transpose',  type=int, default=0, help='')
+
+args = parser.parse_args()
+file_tag = args.file_tag
+x_col = args.x_col
+y_col = args.y_col
+shift = args.shift
+scale = args.scale
+transpose = bool(args.transpose)
 
 len_tag = len(file_tag)
 
@@ -45,24 +58,41 @@ for iter_ in iters:
    with open(files[iter_], "r") as foo:
       foo.readline() # skip first
       for ix in range(nx):
-         y_val = foo.readline().split()[y_col]
+         y_val = float(foo.readline().split()[y_col])
+         y_val += shift
+         y_val *= scale
          y_file.append(float(y_val))
    y_vals[iter_] = y_file
    #print(iter_, files[iter_])
 
 
 # export_csv
-file_out = y_header+".csv"
-print("Exporting to file: %s" % (file_out))
-with open(file_out, "w") as goo:
-   for iter_ in iters:
-      goo.write(",%15d" % (iter_))
-   goo.write("\n")
-   for ix in range(nx):
-      goo.write("%15.10f" % (x_vals[ix]))
-      for iter_ in iters:
-         goo.write(",%15.10f" % (y_vals[iter_][ix]))
-      goo.write("\n")
-      
-      
 
+if not transpose:
+   file_out = y_header+".csv"
+   print("Exporting to file: %s" % (file_out))
+   with open(file_out, "w") as goo:
+      goo.write("%15s" % (""))
+      for iter_ in iters:
+         goo.write(",%15d" % (iter_))
+      goo.write("\n")
+      for ix in range(nx):
+         goo.write("%15.10f" % (x_vals[ix]))
+         for iter_ in iters:
+            goo.write(",%15.10f" % (y_vals[iter_][ix]))
+         goo.write("\n")
+      
+if transpose:
+   file_out = y_header+".csv"
+   print("Exporting to file: %s" % (file_out))
+   with open(file_out, "w") as goo:
+      goo.write("%15s" % (""))
+      for ix in range(nx):
+         goo.write(",%15.10f" % (x_vals[ix]))
+      goo.write(",\n")
+      for iter_ in iters:
+         goo.write("%15d" % (iter_))
+         for ix in range(nx):
+            goo.write(",%15.10f" % (y_vals[iter_][ix]))
+         goo.write(",\n")
+      
